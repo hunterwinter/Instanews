2c9546fc8b4aa396ef009b273c21a22b6047ce05
hunterwinter <hunter_w@live.ca>
2016-05-02 23:08:12 +0000

    script nearly finished.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,66 @@
+ var $grid = $('.grid');
 
-console.log('whyyy');
-console.log('scumbag');
+$(document).ready(function() {
+ 
+  $('select').on('change', function() {
+    var sections = $('.sections :selected').val();
+   
+  $('.loading').show();
+
+  $('.picturelist').empty();
+ 
+  $.getJSON('http://api.nytimes.com/svc/topstories/v1/' + sections + '.json?api-key=0bba64eadd85cae4c4718bcca20ce3ab:8:75124074')
+  
+  .done(function(data) {
+    var results = data.results;
+    var stuff = "";
+    var number = 0;
+
+    console.log(results);
+
+      if (results.length === 0) {
+        $('.option-menu').append('<p class="append">No stories found.</p>');
+      }
+      else{
+    
+        // if (results.length !== 0) {
+
+           results = results.filter(function(item) {
+            return item.multimedia.length;
+          }).splice(0,12);
+        // }
+
+        results.forEach(function(item,  index){
+        var img = item.multimedia[4];
+console.log(img);
+        var abst = item.abstract;
+        var story = item.url;
+        $grid.append('<li class= "box-' + index + '"> <a class="text" href="' + story + '"><p class="writing"> ' + abst +'</p></a> </li>');
+            
+            $('.box-' + index).css("background-image", "url('" + img.url + "')");
+        });
+      }
+  })
+  .always(function() {
+        $('.loading').hide();
+      });
+});
+       
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
